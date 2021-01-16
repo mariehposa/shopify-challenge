55f16796c49e85e54a6cedfeef88a8045868b626
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-16 14:55:53 +0100

    connect store to component

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import { connect } from "react-redux";
+import { addNomination } from "../redux/nominations/actionCreators";
 
-export default function MovieCard({movie}) {
-    return (
-        <div>
-            <img src={movie.Poster} alt={movie.Title}/>
-            <p>{movie.Title}</p>
-            <p>{movie.Year}</p>
-            <p>{movie.Type}</p>
-            <button>Nominate</button>
-        </div>
-    )
-}
+const MovieCard = (props) => {
+  const { movie, addNomination, nominations, action = "add" } = props;
+
+  const handleNominate = (movie) => {
+    addNomination(movie);
+  };
+  return (
+    <div>
+      <img src={movie.Poster} alt={movie.Title} />
+      <p>{movie.Title}</p>
+      <p>{movie.Year}</p>
+      <p>{movie.Type}</p>
+      <button
+        disabled={
+          nominations.map((e) => e.imdbID).includes(movie.imdbID) &&
+          action === "add"
+        }
+        onClick={() => handleNominate(movie)}
+      >
+        {action === "add" ? "Nominate" : "Remove"}
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  nominations: state.nominations.nominations,
+  loading: state.loading,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, {
+  addNomination: (z) => addNomination(z),
+})(MovieCard);
