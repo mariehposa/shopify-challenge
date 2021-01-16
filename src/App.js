5246341830d5d83a5f02aa729f426981f5db549c
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-16 07:26:24 +0100

    add route to compenents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Route } from "react-router-dom";
 import MovieList from './components/movieList';
+import NominationList from './components/nominationList';
 
 function App() {
   return (
     <div>
-      <MovieList />
+      <Route exact path="/" component={MovieList} />
+      <Route path="/nomination_list" component={NominationList} />
     </div>
   );
 }
