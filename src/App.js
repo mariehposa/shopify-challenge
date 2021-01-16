fda75af1098f27d9a7e240aefb46e84af8b92c3a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-16 12:09:41 +0100

    update movielist

--- src/App.js
+++ src/App.js
@@ -2,23 +2,9 @@ import { Route } from "react-router-dom";
 import MovieList from './components/movieList';
 import NominationList from './components/nominationList';
 import Header from './components/Header';
-import { combineReducers, compose, createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk';
-import * as reducers from './redux/reducer';
 
-const totalReducer = combineReducers({
-  aState: reducers.countReducer
-})
-
-const store = createStore(
-  totalReducer,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-)
+import store from './store'
 
 function App() {
   return (
--- src/components/movieList.js
+++ src/components/movieList.js
@@ -1,39 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import MovieCard from "./movieCard";
 import SearchForm from "./searchForm";
+import { getMovieList } from "../redux/movies/actionCreators";
 
 const apiUrl = `http://www.omdbapi.com/?s=ash&apikey=${process.env.REACT_APP_API_KEY}`
 
-const MovieList = () => {
-    const [ moviesList, setMoviesList ] = useState([]);
+const MovieList = (props) => {
+    console.log(props)
+    const { fetchMovies, movieList   } = props;
     const [url, setUrl ] = useState(apiUrl);
 
-    const search = (formValues, actions) =>{
-        const title = formValues.title.toLowercase();
-        const newUrl = `http://www.omdbapi.com/?s=${title}&apikey=${process.env.REACT_APP_API_KEY}`
-        console.log(newUrl);
-        setUrl(newUrl)
-    }
-
     useEffect(() => {
-        axios.get(apiUrl)
-            .then(res => {
-                console.log(res.data);
-                setMoviesList(res.data.Search)
-            })
-            .catch(err => {
-                // console.log(err.message)
-            })
-    }, [url])
+        fetchMovies()
+    }, [url, fetchMovies])
 
     return (
         <div>
-            <SearchForm onSubmit={search} />
+            <SearchForm />
             {
-                moviesList.map(movie => <MovieCard key={movie.imdbID} movie={movie} />)
+                movieList.map(movie => <MovieCard key={movie.imdbID} movie={movie} />)
             }
         </div>
     )
 }
 
-export default MovieList;
+const mapStateToProps = state => ({
+    movieList: state.movies.movieList,
+    loading: state.movies.loading,
+    error: state.movies.error,
+})
+
+
+export default connect(mapStateToProps, {
+    fetchMovies: getMovieList
+})(MovieList);
