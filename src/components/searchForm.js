a11dddfdf81cf4ee3a1c76a739b29a2de6ac382e
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-16 06:21:10 +0100

    create search form

---
+++ src/components/searchForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as yup from "yup";
+
+const initialUserForm = {
+  title: '',
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+      errors.title = 'Please enter a valid name!';
+  }
+}
+
+const validation = yup.object().shape({
+  title: yup.string().required('Please enter correct name!'),
+})
+
+export default function SearchForm() {
+  return (
+    <section>
+
+     <Formik
+            validationSchema= {validation}
+            initialValues={initialUserForm}
+            validate= {validate}
+            onSubmit={}
+            render={props => {
+                return (
+                    <Form>
+                        <label>
+                            <input name='title' type='text' placeholder='Search for stylist...' />
+                            <ErrorMessage name='title' component='div' />
+                        </label>
+                        <button type='submit'>Search</button>
+                    </Form>
+                )
+            }}
+        />
+    </section>
+  );
+}
