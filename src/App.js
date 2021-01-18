ac631876acf355ef9c40088930e7fb44a2d5b18c
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-18 08:50:41 +0100

    use redux persist to hold state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import MovieList from "./components/movieList";
 import NominationList from "./components/nominationList";
 import Header from "./components/Header";
 import { Provider } from "react-redux";
-import store from "./store";
 import "./styles/header.scss";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor, store } from "./store";
+// import LoadingView from "./components";
 
 function App() {
   const [isSticky, setSticky] = useState(false);
   const ref = useRef(null);
+
   const handleScroll = () => {
     if (ref.current) {
       setSticky(ref.current.getBoundingClientRect().top <= 0);
@@ -18,7 +21,6 @@ function App() {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", () => handleScroll);
     };
@@ -26,13 +28,18 @@ function App() {
 
   return (
     <Provider store={store}>
-      <div>
-        <div className={`sticky-wrapper${isSticky ? " sticky" : ""}`} ref={ref}>
-          <Header />
+      <PersistGate loading={<MovieList />} persistor={persistor}>
+        <div>
+          <div
+            className={`sticky-wrapper${isSticky ? " sticky" : ""}`}
+            ref={ref}
+          >
+            <Header />
+          </div>
+          <Route exact path={["/", "/movies"]} component={MovieList} />
+          <Route path="/nomination_list" component={NominationList} />
         </div>
-        <Route exact path={["/", "/movies"]} component={MovieList} />
-        <Route path="/nomination_list" component={NominationList} />
-      </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,31 @@
 import { combineReducers, compose, createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk';
-import moviesReducer from '../redux/movies/reducer'
-import nominationsReducer from '../redux/nominations/reducer';
+import thunk from "redux-thunk";
+import moviesReducer from "../redux/movies/reducer";
+import nominationsReducer from "../redux/nominations/reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+};
 
 const allReducers = combineReducers({
-    movies: moviesReducer,
-    nominations: nominationsReducer,
-  })
-  
-  const store = createStore(
-    allReducers,
-    {},
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-  )
+  movies: moviesReducer,
+  nominations: nominationsReducer,
+});
 
-export default store
+const pReducer = persistReducer(persistConfig, allReducers);
+
+export const store = createStore(
+  pReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export const persistor = persistStore(store);
